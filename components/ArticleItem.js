24bf6b9dda0a2d9bab36876266d171478986afb4
ohhamma <81293930+ohhamma@users.noreply.github.com>
2022-07-17 00:17:51 +0900

    styled-components added..?

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,15 +1,30 @@
 import Link from 'next/link'
 import articleStyles from '../styles/Article.module.css'
+import styled from 'styled-components'
 
 const ArticleItem = ({article}) => {
   return (
     <Link href="/article/[id]" as={`/article/${article.id}`}>
       <a className={articleStyles.card}>
         <h3>{article.title} &rarr;</h3>
+        {/* <ArticleTitle>{article.title} &rarr;</ArticleTitle> */}
         <p>{article.excerpt}</p>
+        {/* <ArticleBody>{article.excerpt}</ArticleBody> */}
       </a>
     </Link>
   )
 }
+
+// using styled-components
+const ArticleTitle = styled.div`
+  margin: 0 0 1rem 0;
+  font-size: 1.5rem;
+`;
+
+const ArticleBody = styled.div`
+  margin: 0;
+  font-size: 1.25rem;
+  line-height: 1.5;
+`;
 
 export default ArticleItem
--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,14 +1,27 @@
 import ArticleItem from './ArticleItem'
 import articleStyles from '../styles/Article.module.css'
+import styled from 'styled-components'
 
 const ArticleList = ({articles}) => {
   return (
-    <div className={articleStyles.grid}>
+    // <div className={articleStyles.grid}>
+    <ArticleGrid>
       {articles.map((article) => (
         <ArticleItem article={article} />
-      ))}
-    </div>
+        ))}
+    </ArticleGrid>
+    // </div>
   )
 }
+
+// using styled-components
+const ArticleGrid = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 800px;
+  margin-top: 3rem;
+`;
 
 export default ArticleList
